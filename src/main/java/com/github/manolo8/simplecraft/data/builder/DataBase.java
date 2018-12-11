0ea60114519cc6244446b458de4152cd53bc17db
Willian <willianmaysoares@gmail.com>
2018-12-11 20:05:55 -0200

    update

--- src/main/java/com/github/manolo8/simplecraft/data/builder/DataBase.java
+++
@@ -1,8 +0,0 @@
-package com.github.manolo8.simplecraft.data.builder;
-
-import java.sql.Connection;
-
-public interface DataBase {
-
-    Connection getConnection();
-}
--- src/main/java/com/github/manolo8/simplecraft/modules/skill/tools/JoinSkill.java
+++
@@ -1,8 +0,0 @@
-package com.github.manolo8.simplecraft.modules.skill.tools;
-
-import com.github.manolo8.simplecraft.modules.user.User;
-
-public interface JoinSkill {
-
-    void onJoin(User user);
-}
